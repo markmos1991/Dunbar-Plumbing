643ee818532e72c0a94114e5fa6525fab9fffb95
markmos1991 <101826190+markmos1991@users.noreply.github.com>
2023-08-31 16:03:46 +0100

    added sections

---
+++ dunbar/components/footer.tsx
@@ -0,0 +1,17 @@
+
+  
+  export default function Example() {
+    return (
+      <footer className="bg-white">
+        <div className="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8">
+         
+          <div className="mt-8 md:order-1 md:mt-0">
+            <p className="text-center text-xs leading-5 text-gray-500">
+              &copy; 2020 Your Company, Inc. All rights reserved.
+            </p>
+          </div>
+        </div>
+      </footer>
+    )
+  }
+  
